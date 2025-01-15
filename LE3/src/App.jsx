58127dde28dae206f868626ff7cb964844ef7ba6
wolz4k <glennchristianegopez@gmail.com>
2025-01-16 02:10:39 +0800

    Converted channel column to components

--- LE3/src/App.jsx
+++ LE3/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ServerPic from "./components/ServerPic";
 import "./App.css";
 import accIcon from "./assets/img/acc-icon.png";
+import { ChannelDropdown, ChannelList, Channel } from "./components/Channel";
 
 function App() {
   return (
@@ -97,48 +98,37 @@ function App() {
           </div>
         </div>
 
-        <div className="h-auto grow bg-base-100">
-          <ul className="menu bg-base-100 font-medium
-          [&_li>a]:py-1
-          ">
-            <li>
-              <a href="">
-              <svg className="h-5 w-5 "  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="4" y1="9" x2="20" y2="9" />  <line x1="4" y1="15" x2="20" y2="15" />  <line x1="10" y1="3" x2="8" y2="21" />  <line x1="16" y1="3" x2="14" y2="21" /></svg>
-                General
-              </a>
-            </li>
-            <li>
-              <a href="">
-              <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
-              </svg>
-                General
-              </a>
-            </li>
-            <li>
-              <details open>
-                <summary className="uppercase text-xs">Server Boost</summary>
-                <ul className="ms-0 ps-0">
-                  <li>
-                    <a href="">
-                    <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
-                    </svg>
-                      Boost</a>
-                  </li>
-                  <li>
-                    <a href="">
-                    <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
-                    </svg>
-                      Boost</a>
-                  </li>
-                </ul>
+        <ChannelList>
+          <ChannelDropdown dropName="Channels">
+            <Channel text={false} channel="General" />
+          </ChannelDropdown>
+          <ChannelDropdown dropName="Channels">
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+            <Channel text={false} channel="General" />
+          </ChannelDropdown>
+        </ChannelList>
 
-              </details>
-            </li>
-          </ul>
-        </div>
+        
         <div className="flex w-full p-2 bg-base-100">
           <div className="btn btn-ghost p-1 items-center grow justify-start">
             <div className="avatar indicator">
