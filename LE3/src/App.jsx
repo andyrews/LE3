da7f7d795c80f3e430b9774e125dadae8bd7c98b
wolz4k <glennchristianegopez@gmail.com>
2025-01-16 00:05:44 +0800

    added channels and groups

--- LE3/src/App.jsx
+++ LE3/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="flex w-screen h-screen">
       {/*Navbar Servers*/}
-      <div className="min-w-[70px] h-full bg-base-100">
+      <div className="min-w-[70px] h-full bg-base-200">
         <div className="flex h-full flex-col">
           {/*Server List*/}
           <div className="overflow-y-auto overflow-x-hidden h-full w-full scrollbar-thin scrollbar-webkit">
@@ -84,8 +84,8 @@ function App() {
       </div>
 
       {/*Server Channels*/}
-      <div className="w-72 h-full bg-accent">
-        <div className="navbar h-12 min-h-0 px-4 bg-base-100">
+      <div className="w-72 h-full bg-neutral">
+        <div className="navbar h-12 min-h-0 px-4 bg-base-100 drop-shadow-2xl">
           <div className="flex-1 font-extrabold">
           toothbrush/ice pick
           </div>
@@ -96,13 +96,49 @@ function App() {
           </div>
         </div>
 
-        <ul className="menu bg-base-200">
-          <li>
-            <a href="">
-              General
-            </a>
-          </li>
-        </ul>
+        <div className="h-auto">
+          <ul className="menu bg-base-100 font-medium
+          [&_li>a]:py-1
+          ">
+            <li>
+              <a href="">
+              <svg className="h-5 w-5 "  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="4" y1="9" x2="20" y2="9" />  <line x1="4" y1="15" x2="20" y2="15" />  <line x1="10" y1="3" x2="8" y2="21" />  <line x1="16" y1="3" x2="14" y2="21" /></svg>
+                General
+              </a>
+            </li>
+            <li>
+              <a href="">
+              <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
+              </svg>
+                General
+              </a>
+            </li>
+            <li>
+              <details open>
+                <summary className="uppercase text-xs">Server Boost</summary>
+                <ul className="ms-0 ps-0">
+                  <li>
+                    <a href="">
+                    <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
+                    </svg>
+                      Boost</a>
+                  </li>
+                  <li>
+                    <a href="">
+                    <svg class="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
+                    </svg>
+                      Boost</a>
+                  </li>
+                </ul>
+
+              </details>
+            </li>
+          </ul>
+        </div>
+        
 
       </div>
 
